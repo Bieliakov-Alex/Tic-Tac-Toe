baaa22045cf8d6e7e2a6ab421d299a2033a18d43
Alex Bieliakov <bieliakov.alex@gmail.com>
2018-01-25 18:14:15 +0200

    Добавил класс TTTGame, который описывает игру "Крестики-нолики"

---
+++ Tic-Tac-Toe-Game/Tic-Tac-Toe-Game/TTTGame.cpp
@@ -0,0 +1,210 @@
+﻿#include "TTTGame.h"
+
+
+
+void TTTGame::setLength(int length)
+{
+	if (length > 0)
+	{
+		this->length = length;
+	}
+	else
+	{
+		this->length = 3;
+	}
+}
+
+void TTTGame::setWidth(int width)
+{
+	if (width>0)
+	{
+		this->width = width;
+	}
+	else
+	{
+		this->width = 3;
+	}
+}
+
+void TTTGame::setCross(char cross)
+{
+	if (cross != '_')
+	{
+		this->cross = cross;
+	}
+	else
+	{
+		this->cross = 'x';
+	}
+}
+
+void TTTGame::setToe(char toe)
+{
+	if (toe != '_')
+	{
+		this->toe = toe;
+	}
+	else
+	{
+		this->toe = 'o';
+	}
+}
+
+void TTTGame::setCountElements(int countElements)
+{
+	if (countElements>0)
+	{
+		this->countElements = countElements;
+	}
+	else
+	{
+		this->countElements = 3;
+	}
+}
+
+TTTGame::TTTGame(int length, int width, int countElements, char cross, char toe)
+{
+	this->setLength(length);
+	this->setWidth(width);
+	this->setCountElements(countElements);
+	this->setCross(cross);
+	this->setToe(toe);
+
+	//выделяем память под поле
+	this->field = new char*[this->getLength()];
+	for (size_t i = 0; i < this->getLength(); i++)
+	{
+		this->field[i] = new char[this->getWidth()];
+	}
+
+	//заполняем поле пробелами
+	for (size_t i = 0; i < this->getLength(); i++)
+	{
+		for (size_t j = 0; j < this->getWidth(); j++)
+		{
+			this->field[i][j] = '_';
+		}
+	}
+
+	this->gameEnd = false;
+	this->gameState = CONTINUE;
+}
+
+TTTGame::~TTTGame()
+{
+	for (size_t i = 0; i < this->getLength(); i++)
+	{
+		delete[] this->field[i];
+	}
+	delete this->field;
+}
+
+const char ** TTTGame::getField() const
+{
+	return (const char**)field;
+}
+
+int TTTGame::getLength() const
+{
+	return this->length;
+}
+
+int TTTGame::getWidth() const
+{
+	return this->width;
+}
+
+bool TTTGame::firstPlayerMove(int length, int width)
+{
+	return false;
+}
+
+bool TTTGame::secondPlayerMove(int length, int width)
+{
+	return false;
+}
+
+void TTTGame::calculateState()
+{
+	if (this->checkDraw())
+	{
+		this->gameEnd = true;
+		this->gameState = DRAW;
+	}
+	else
+	{
+		//TODO: реализовать проверку победы игроков
+	}
+}
+
+bool TTTGame::checkDraw()const
+{
+	for (size_t i = 0; i < this->getLength(); i++)
+	{
+		for (size_t j = 0; j < this->getWidth(); j++)
+		{
+			if (this->field[i][j] == '_') return false;
+		}
+	}
+
+	return true;
+}
+
+bool TTTGame::checkWin(char symbol) const
+{
+	return false;
+}
+
+bool TTTGame::checkHorizontal(char symbol) const
+{
+	for (size_t i = 0; i < this->getLength(); i++)
+	{
+		for (size_t j = 0; j < this->getWidth()-this->countElements+1; j++)
+		{
+			if (this->field[i][j] == symbol)
+			{
+				bool symbolWin = true;
+				for (size_t k = j+1; k < j+countElements; k++)
+				{
+					if (this->field[i][k] != symbol)
+					{
+						symbolWin = false;
+					}
+				}
+				if (symbolWin)
+				{
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+bool TTTGame::checkVertical(char symbol) const
+{
+	for (size_t i = 0; i < this->getLength()-this->countElements+1; i++)
+	{
+		for (size_t j = 0; j < this->getWidth(); j++)
+		{
+			if (this->field[i][j] == symbol)
+			{
+				bool symbolWin = true;
+				for (size_t k = i+1; k < countElements+i; k++)
+				{
+					if (this->field[k][j] != symbol)
+					{
+						symbolWin = false;
+					}
+				}
+				if (symbolWin)
+				{
+					return true;
+				}
+			}
+		}
+	}
+
+	return false;
+}
---
+++ Tic-Tac-Toe-Game/Tic-Tac-Toe-Game/TTTGame.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+//перечисление состояния игры
+//CONTINE - игра продолжается
+//FIRST_PLAYER_WIN - победил первый игрок
+//SECOND_PLAYER_WIN - победил второй игрок
+//DRAW - ничья
+enum GameState {CONTINUE, FIRST_PLAYER_WIN, SECOND_PLAYER_WIN, DRAW};
+
+class TTTGame
+{
+private:
+	//размер поля по вертикали
+	int length;
+	//размер поля по горизонтали
+	int width;
+	//поле
+	char** field;
+	//символ крестика
+	char cross;
+	//символ нолика
+	char toe;
+	//признак конца игры
+	bool gameEnd;
+	//количество элементов в ряд для победы
+	int countElements;
+	//состояние игры - продолжается, победа первого\второго игрока, ничья
+	GameState gameState;
+
+	void setLength(int length);
+	void setWidth(int width);
+	void setCross(char cross);
+	void setToe(char toe);
+	void setCountElements(int countElements);
+
+public:
+	TTTGame(int length=3, int width=3, int countElements=3, char cross='x', char toe='o');
+	virtual ~TTTGame();
+	const char** getField()const;
+	int getLength()const;
+	int getWidth()const;
+	bool firstPlayerMove(int length, int width);
+	bool secondPlayerMove(int length, int width);
+	void calculateState();
+
+	bool checkDraw()const;
+	bool checkWin(char symbol)const;
+	bool checkHorizontal(char symbol)const;
+	bool checkVertical(char symbol)const;
+	//TODO: Реализовать проверку диагоналей
+};
+
